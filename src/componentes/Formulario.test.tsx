49578666de7ca48b81956796cdd2eb4612577c50
lucasdav <lucassouzadavanso@gmail.com>
2024-03-04 19:18:30 -0300

    buscando elementos na tela

---
+++ src/componentes/Formulario.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react"
+
+
+test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
+    render(<Formulario />)
+
+    // encontrar no DOM o input
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+
+    // encontrar o botão
+    const botao = screen.getByRole('button')
+
+    // garantir que o input esteja no documento
+    expect(input).toBeInTheDocument()
+
+    // garantir que o botão esteja desabilitado
+    expect(botao).toBeDisabled()
+})
