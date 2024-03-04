44004c21642c1e69057e8390fae5a932f72f4b88
lucasdav <lucassouzadavanso@gmail.com>
2024-03-04 19:25:33 -0300

    fazendo o teste passar

--- src/componentes/Formulario.test.tsx
+++ src/componentes/Formulario.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react"
+import Formulario from "./Formulario"
 
 
 test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
