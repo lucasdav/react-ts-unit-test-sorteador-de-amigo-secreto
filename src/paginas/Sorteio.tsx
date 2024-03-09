ef4a0fb039e60f2f06e36b45d902ffb177f48762
lucasdav <lucassouzadavanso@gmail.com>
2024-03-09 13:31:04 -0300

    estilo config css

--- src/paginas/Sorteio.tsx
+++ src/paginas/Sorteio.tsx
@@ -1,13 +1,17 @@
+// src/paginas/Sorteio.tsx
 import { useState } from "react"
+import Card from "../componentes/Card"
 import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes"
 import { useResultadoSorteio } from "../state/hook/useResultadoSorteio"
+
+import './Sorteio.css'
 
 const Sorteio = () => {
 
     const participantes = useListaDeParticipantes()
 
     const [participanteDaVez, setParticipanteDaVez] = useState('')
-    const [amigoSecreto, setAmigoSecreto] = useState('')
+    const [amigoScreto, setAmigoSecreto] = useState('')
 
     const resultado = useResultadoSorteio()
 
@@ -18,8 +22,9 @@ const Sorteio = () => {
         }
     }
 
-    return (
-        <section>
+    return (<Card>
+        <section className="sorteio">
+            <h2>Quem vai tirar o papelzinho?</h2>
             <form onSubmit={sortear}>
                 <select
                     required
@@ -31,11 +36,15 @@ const Sorteio = () => {
                 >
                     {participantes.map(participante => <option key={participante}>{participante}</option>)}
                 </select>
-                <button>Sortear</button>
+                <p>Clique em sortear para ver quem é seu amigo secreto!</p>
+                <button className="botao-sortear">Sortear</button>
             </form>
-            {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
+            {amigoScreto && <p className="resultado" role="alert">{amigoScreto}</p>}
+            <footer className="sorteio">
+                <img src="/imagens/aviao.png" className="aviao" alt="Um desenho de um avião de papel" />
+            </footer>
         </section>
-    )
+    </Card>)
 }
 
 export default Sorteio
