203fa65ba4f5429b95fde66f7b44658ee809fb8d
lucasdav <lucassouzadavanso@gmail.com>
2024-03-09 13:16:59 -0300

    exibindo o amigo secreto

--- src/paginas/Sorteio.test.tsx
+++ src/paginas/Sorteio.test.tsx
@@ -1,11 +1,17 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { RecoilRoot } from "recoil"
 import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes"
 import Sorteio from "./Sorteio"
+import { useResultadoSorteio } from "../state/hook/useResultadoSorteio"
 
 jest.mock('../state/hook/useListaDeParticipantes', () => {
     return {
         useListaDeParticipantes: jest.fn()
+    }
+})
+jest.mock('../state/hook/useResultadoSorteio', () => {
+    return {
+        useResultadoSorteio: jest.fn()
     }
 })
 
@@ -13,10 +19,17 @@ describe('a pagina de sorteio', () => {
     const participantes = [
         'Ana',
         'Catarina',
-        'Joao'
+        'Jorel'
     ]
+    const resultado = new Map([
+        ['Ana', 'Jorel'],
+        ['Jorel', 'Catarina'],
+        ['Catarina', 'Ana']
+
+    ])
     beforeEach(() => {
-        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes)
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+        (useResultadoSorteio as jest.Mock).mockReturnValue(resultado)
     })
     test('todos os participantes podem exibir o seu amigo secreto', () => {
         render(
@@ -27,5 +40,28 @@ describe('a pagina de sorteio', () => {
 
         const opcoes = screen.queryAllByRole('option')
         expect(opcoes).toHaveLength(participantes.length)
+    })
+    test('o amigo secreto é exibido quando solicitado', () => {
+        render(
+            <RecoilRoot>
+                <Sorteio />
+            </RecoilRoot>
+        )
+
+        const select = screen.getByPlaceholderText('Selecione o seu nome')
+
+        fireEvent.change(select, {
+            target: {
+                value: participantes[0]
+            }
+        })
+
+        const botao = screen.getByRole('button')
+
+        fireEvent.click(botao)
+
+        const amigoSecreto = screen.getByRole('alert')
+
+        expect(amigoSecreto).toHaveBeenCalled()
     })
 })
